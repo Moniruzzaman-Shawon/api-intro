fd1fc7a256b7a04083e4ff6189763c46b44ac297
Moniruzzaman Shawon <m.zaman.djp@gmail.com>
2024-01-16 23:17:55 +0600

    style added

--- js/post.js
+++ js/post.js
@@ -6,7 +6,15 @@ function loadPosts(){
 loadPosts();
 
 function displayPosts(posts){
+    const postContainer = document.getElementById('posts');
     for (const post of posts){
+        const div = document.createElement('div');
+        div.classList.add('post');
+        div.innerHTML = `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+        `;
+        postContainer.appendChild(div);
         console.log(post.title);
     }
 }
