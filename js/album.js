af756feabcbb8838c9533877fb0728100d71d1fd
Moniruzzaman Shawon <m.zaman.djp@gmail.com>
2024-01-18 03:42:15 +0600

    comments added with style

--- js/album.js
+++ js/album.js
@@ -6,7 +6,7 @@ function loadAlbums(){
 loadAlbums()
 
 function displayAlbums(albums){
-    console.log(albums);
+    // console.log(albums);
     const albumContainer = document.getElementById('albums');
     for ( const album of albums){
         const p = document.createElement('p');
@@ -14,4 +14,25 @@ function displayAlbums(albums){
         albumContainer.appendChild(p);
     }
 
+}
+function loadComments(){
+    fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(res => res.json())
+    .then(data => displayComments(data))
+}
+loadComments()
+
+function displayComments(comments){
+    // console.log(comments);
+    const commentContainer = document.getElementById('comments');
+    for( const comment of comments){
+        const com = document.createElement('div');
+        com.classList.add('comment');
+        com.innerHTML = `
+            <h3>${comment.email}</h3>
+            <p>${comment.body}</p>
+        `
+        commentContainer.appendChild(com);
+    
+    }
 }
